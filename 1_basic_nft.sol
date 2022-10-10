fd94091bc3d1acef400e3472b0126681e9c3cdd6
Gareth Edwards <garrethedwards@gmail.com>
2022-10-10 11:05:05 +0200

    Update 1_basic_nft.sol

--- 1_basic_nft.sol
+++ 1_basic_nft.sol
@@ -2,12 +2,10 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract RtcNft is
-ERC721,
-Ownable
+ERC721
 {
     using Counters for Counters.Counter;
 
